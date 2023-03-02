6dd9be277b779e71e83c643bb045ed5e03ab2ec1
Valokoodari <Valokoodari@users.noreply.github.com>
2023-03-02 20:50:09 +0200

    Fix when server doesn't respond

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,25 @@ async fn main() -> WebhookResult<()> {
     dotenv::dotenv()?;
 
     let website_url = dotenv::var("WEBSITE_URL")?;
-    let response = reqwest::get(&website_url).await?;
+    let response = match reqwest::get(&website_url).await {
+        Ok(response) => response,
+        Err(_) => {
+            return send_message().await;
+        }
+    };
 
     if !response.status().is_success() {
-        let webhook_url = dotenv::var("WEBHOOK_URL")?;
-        let client = WebhookClient::new(&webhook_url);
-
-        client.send(|message|
-            message.content("Website is down!").username("Website status")
-        ).await?;
+        return send_message().await;
     }
+
+    Ok(())
+}
+
+async fn send_message() -> WebhookResult<()> {
+    let webhook_url = dotenv::var("WEBHOOK_URL")?;
+    let client = WebhookClient::new(&webhook_url);
+
+    client.send(|message| message.content("Website is down!").username("Website status")).await?;
 
     Ok(())
 }
